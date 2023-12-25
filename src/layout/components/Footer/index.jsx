adbbf88f3233283c73789f946b7146d3330c3f03
Farshid Enjili <f.enjili@fwutech.com>
2023-12-25 19:14:27 +0330

    fix-footer

--- src/layout/components/Footer/index.jsx
+++ src/layout/components/Footer/index.jsx
@@ -1,14 +1,39 @@
+import { Col, Row } from "antd";
+
+import "./style.css";
+import img from "./test.jpg";
+
 const MainFooter = () => {
 	return (
-		<div className="grid grid-rows-4 grid-flow-col gap-2 h-[300px]">
-			<div className="row-span-2 col-span-2 h-full w-full rounded-2xl bg-amber-100"></div>
-			<div className="row-span-2 col-span-2 h-full w-full rounded-2xl bg-amber-100"></div>
-			<div className="row-span-4 col-span-1 h-full w-full rounded-2xl bg-blue-200"></div>
-			<div className="row-span-4 col-span-1 h-full w-full rounded-2xl bg-blue-200"></div>
-			<div className="row-span-3 col-span-1 h-full w-full rounded-2xl bg-purple-200"></div>
-			<div className="row-span-1 col-span-2 h-full w-full rounded-2xl bg-slate-200"></div>
-			<div className="row-span-3 col-span-1 h-full w-full rounded-2xl bg-purple-200"></div>
-		</div>
+		<Row gutter={[8, 8]} className="text-center">
+			<Col xs={24} lg={8} className="bg-red-100 rounded-2xl"></Col>
+			<Col xs={24} lg={5} className="bg-green-100 rounded-2xl"></Col>
+			<Col xs={24} lg={5} className="bg-blue-100 rounded-2xl"></Col>
+			<Col xs={24} lg={6}>
+				<Row gutter={[2,2]}>
+					<Col span={12} className="img-fill">
+						<img
+							src={img}
+							alt="guaranty-us-img"
+							className="rounded-lg"
+							width={150}
+							height={200}
+							loading="lazy"
+						/>
+					</Col>
+					<Col span={12} className="img-fill">
+						<img
+							src={img}
+							alt="guaranty-u-img"
+							className="rounded-lg"
+							width={150}
+							height={200}
+							loading="lazy"
+						/>
+					</Col>
+				</Row>
+			</Col>
+		</Row>
 	);
 };
 
